79669aa16c4c72e2b2c52fe94dc7169023c3da6e
isaac-silv <isaacaquino.dv@gmail.com>
2022-11-21 11:15:29 -0300

    fechando-as-rotas

--- dist/routes/alunoRoutes.js
+++ dist/routes/alunoRoutes.js
@@ -5,10 +5,10 @@ var _loginRequired = require('../middlewares/loginRequired'); var _loginRequired
 
 const router = new (0, _express.Router)();
 
-router.get('/', _AlunoController2.default.index);
+router.get('/', _loginRequired2.default, _AlunoController2.default.index);
 router.post('/', _loginRequired2.default, _AlunoController2.default.store);
 router.put('/:id', _loginRequired2.default, _AlunoController2.default.update);
-router.get('/:id', _AlunoController2.default.show);
+router.get('/:id', _loginRequired2.default, _AlunoController2.default.show);
 router.delete('/:id', _loginRequired2.default, _AlunoController2.default.delete);
 
 exports. default = router;
--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -5,10 +5,10 @@ import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
-router.get('/', alunoController.index);
+router.get('/', loginRequired, alunoController.index);
 router.post('/', loginRequired, alunoController.store);
 router.put('/:id', loginRequired, alunoController.update);
-router.get('/:id', alunoController.show);
+router.get('/:id', loginRequired, alunoController.show);
 router.delete('/:id', loginRequired, alunoController.delete);
 
 export default router;
