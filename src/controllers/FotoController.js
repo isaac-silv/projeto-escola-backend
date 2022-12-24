bb1b07060ee49e8748849ec615e6833fe052c00e
isaac-silv <isaacaquino.dv@gmail.com>
2022-12-24 10:44:39 -0300

    Configurando o multer & firebase storage

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,64 +1,21 @@
-import admin from 'firebase-admin'
-import multer from 'multer';
-import multerConfig from '../config/multerConfig';
-
 import Foto from '../models/Foto';
 
-const upload = multer(multerConfig).single('foto');
+const store = async (req, res) => { 
+  console.log('file', req.file)
+  
+  try {
+    const { originalname, path } = req.file;   
+    const { aluno_id } = req.body;
+    console.log('originalname', );    
 
-const serviceAccount = require("../config/firebaseConfig.json");
+    const foto = await Foto.create({ originalname, filename: path, aluno_id });
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-});
-
-const bucket = admin.storage().bucket();
-
-const store = async (req, res) => {
-  return upload(req, res, async (error) => {
-    console.log('file', req.file)
-    if (error) {
-      return res.status(400).json({
-        errors: [error.code],
-      });
-    }
-
-    try {
-      const { originalname } = req.file;
-      const filename = Date.now() + "." + originalname.split(".").pop();
-      const { aluno_id } = req.body;
-
-      const file = bucket.file(filename);
-
-      const stream = file.createWriteStream({
-        metadata: {
-          contentType: req.file.mimetype,
-        },
-      });
-
-      stream.on("error", (e) => {
-        console.error(e);
-      });
-    
-      stream.on("finish", async () => {
-        //tornar o arquivo publico
-        await file.makePublic();
-             
-        next();
-      });
-    
-      stream.end(foto.buffer);
-
-      const foto = await Foto.create({ originalname, filename, aluno_id });
-
-      return res.json(foto);
-    } catch (e) {
-      return res.status(400).json({
-        errors: ['Aluno não existe'],
-      });
-    }
-  });
+    return res.json(foto);
+  } catch (e) {
+    return res.status(400).json({
+      errors: ['Aluno não existe'],
+    });
+  }
 }
 
 export const fotoController = {
--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -1,10 +1,25 @@
 import { Router } from 'express';
 import loginRequired from '../middlewares/loginRequired';
+import FirebaseStorage from 'multer-firebase-storage';
 
 import { fotoController } from '../controllers/FotoController';
+import multer from 'multer';
 
 const router = new Router();
 
-router.post('/', loginRequired, fotoController.store);
+const  Multer  =  multer({ 
+    storage: FirebaseStorage({ 
+      bucketName: process.env.FIREBASE_STORAGE_BUCKET, 
+      credentials: { 
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), 
+        projectId: process.env.FIREBASE_PROJECT_ID 
+      },
+      unique: true,
+      public: true
+    }) 
+  } )
+
+router.post('/', loginRequired, Multer.single('foto'), fotoController.store);
 
 export default router;
